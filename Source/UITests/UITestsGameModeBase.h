7febab254edd2f3572469718c702dc20b9017f6c
culorin <culorin@users.noreply.github.com>
2023-07-05 17:15:23 +0200

    Added stripped down project files

---
+++ Source/UITests/UITestsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UITestsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UITESTS_API AUITestsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
