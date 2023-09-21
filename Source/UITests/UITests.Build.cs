27834896adcd20191ee207a23a03625a5f585628
culorin <culorin@users.noreply.github.com>
2023-09-21 18:02:44 +0200

    Ink integration test

--- Source/UITests/UITests.Build.cs
+++ Source/UITests/UITests.Build.cs
@@ -8,7 +8,7 @@ public class UITests : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Ink", "InkEditor", "Ink", "InkEditor" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
